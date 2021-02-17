e31b9482cd2b13cafa25dbb96c8c100cc1887dde
samprasgirt <14274044@bjtu.edu.cn>
2021-02-17 11:56:15 +0800

    update

---
+++ LeetcodeGo/703 数据流中第k大元素/KthLargest.go
@@ -0,0 +1,5 @@
+package LeetCodeGo
+
+func KthLargest() {
+	
+}
--- LeetcodeGo/978 最长湍流子数组/maxTurbulenceSize.go
+++ LeetcodeGo/978 最长湍流子数组/maxTurbulenceSize.go
@@ -1,6 +1,38 @@
 package LeetCodeGo
 
-func maxTurbulenceSize(arr[] int) int {
+func max(A int, B int) int {
+	if A > B {
+		return A
+	}
+	return B
+}
+func min(A int, B int) int {
+	if A < B {
+		return A
+	}
+	return B
+}
 
-	
+// 动态规划方法
+func maxTurbulenceSize(arr []int) int {
+	n := len(arr)
+	up := 1
+	down := 1
+	res := min(1, n)
+	for i := 1; i < n; i++ {
+		if arr[i-1] < arr[i] {
+			up = down + 1
+			down = 1
+		} else if arr[i-1] > arr[i] {
+			down = up + 1
+			up = 1
+		} else {
+			down = 1
+			up = 1
+		}
+		res = max(res, max(down, up))
+
+	}
+	return res
+
 }
